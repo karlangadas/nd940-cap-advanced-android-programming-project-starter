0ebdab35c04fc1967245964fc4efb949b6216e80
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-11-19 15:20:47 +0000

    representatives

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+import com.example.android.politicalpreparedness.election.ElectionsViewModel
 import com.example.android.politicalpreparedness.network.models.Address
 import java.util.Locale
 
@@ -16,21 +20,26 @@ class DetailFragment : Fragment() {
         //TODO: Add Constant for Location request
     }
 
-    //TODO: Declare ViewModel
+    lateinit var viewModel: RepresentativeViewModel
 
-//    override fun onCreateView(inflater: LayoutInflater,
-//                              container: ViewGroup?,
-//                              savedInstanceState: Bundle?): View? {
-//
-//        //TODO: Establish bindings
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding = FragmentRepresentativeBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(this)[RepresentativeViewModel::class.java]
+
+        binding.viewModel = viewModel
 //
 //        //TODO: Define and assign Representative adapter
 //
 //        //TODO: Populate Representative adapter
 //
 //        //TODO: Establish button listeners for field and location search
-//
-//    }
+        return binding.root
+    }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
